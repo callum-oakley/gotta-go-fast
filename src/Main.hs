d817a8a962bb71ba615e85a84a3412c174ffe40c
Callum Oakley <callum@pusher.com>
2017-07-22 23:59:23 +0100

    wrap lines

--- src/Main.hs
+++ src/Main.hs
@@ -16,10 +16,17 @@ maxNoOfLines = 30
 trimEmptyLines :: [String] -> [String]
 trimEmptyLines = reverse . dropWhile (== "") . reverse . dropWhile (== "")
 
--- TODO wrap lines instead of cutting them off at maxLineLength
-strip :: String -> IO String
-strip file = do
-  let ls = map (take maxLineLength) $ lines file -- wrap at this point
+chunksOf :: Int -> [a] -> [[a]]
+chunksOf n xs
+  | length xs <= n = [xs]
+  | otherwise = take n xs : chunksOf n (drop n xs)
+
+wrapAt :: Int -> String -> String
+wrapAt n s = unlines $ concatMap (chunksOf n) $ lines s
+
+sample :: String -> IO String
+sample file = do
+  let ls = lines $ wrapAt maxLineLength file
   -- For files longer than maxNoOfLines we grab a random segment.
   r <- randomRIO (0, max 0 $ length ls - maxNoOfLines)
   return $ unlines $ trimEmptyLines $ take maxNoOfLines $ drop r ls
@@ -35,5 +42,5 @@ main = do
     _ -> do
       r <- randomRIO (0, length files - 1)
       file <- readFile $ files !! r
-      stripped <- strip file
-      run stripped
+      sampled <- sample file
+      run sampled
