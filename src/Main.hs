7759a77b5140293c31d1421944970e7d758c96f6
Callum Oakley <callum@pusher.com>
2017-07-23 12:28:35 +0100

    move wrapAt to its own module

--- src/Main.hs
+++ src/Main.hs
@@ -6,6 +6,7 @@ import System.Environment (getArgs, getProgName)
 import System.Random (randomRIO)
 
 import UI
+import WordWrap (wrapAt)
 
 maxLineLength :: Int
 maxLineLength = 80
@@ -15,38 +16,6 @@ maxNoOfLines = 30
 
 trimEmptyLines :: [String] -> [String]
 trimEmptyLines = reverse . dropWhile (== "") . reverse . dropWhile (== "")
-
-chunksOf :: Int -> [a] -> [[a]]
-chunksOf n xs
-  | length xs <= n = [xs]
-  | otherwise = take n xs : chunksOf n (drop n xs)
-
-getIndent :: String -> String
-getIndent = takeWhile (== ' ')
-
-ensureEndWithNewline :: String -> String
-ensureEndWithNewline "" = "\n"
-ensureEndWithNewline s
-  | last s == '\n' = s
-  | otherwise = s ++ "\n"
-
--- TODO Replace this atrocity with a library function (which is currently
--- buggy, need to submit a PR).
-wrapAt :: Int -> String -> String
-wrapAt n = result . foldl greedy ("", "", "", "") . ensureEndWithNewline
-  where
-    result (acc, _, _, _) = acc
-    greedy (acc, l, s, w) '\n'
-      | length (l ++ s ++ w) <= n = (acc ++ l ++ s ++ w ++ "\n", "", "", "")
-      | otherwise = (acc ++ l ++ "\n" ++ getIndent l ++ w ++ "\n", "", "", "")
-    greedy (acc, l, s, "") ' ' = (acc, l, s ++ " ", "")
-    greedy (acc, l, s, w) ' '
-      | length (l ++ s ++ w) <= n = (acc, l ++ s ++ w, " ", "")
-      | otherwise = (acc ++ l ++ "\n", getIndent l ++ w, " ", "")
-    greedy (acc, l, s, w) c
-      | length (getIndent l ++ w) < n = (acc, l, s, w ++ [c])
-      | otherwise =
-        (acc ++ l ++ "\n" ++ getIndent l ++ w ++ "\n", getIndent l, "", [c])
 
 sample :: String -> IO String
 sample file = do
