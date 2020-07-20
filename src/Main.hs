23142f6fb902680a45f486c47fec3ce22571703c
Callum Oakley <hello@callumoakley.net>
2020-07-20 21:22:16 +0100

    generate statistically convincing nonsense

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,11 @@
 {-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE TemplateHaskell    #-}
 
 module Main where
 
 import           Control.Monad          (filterM)
 import           Data.Char              (isAscii, isPrint)
+import           Data.FileEmbed         (embedStringFile)
 import           Data.List.Split        (splitOn)
 import qualified Data.Text              as T
 import           Data.Word              (Word8)
@@ -83,6 +85,42 @@ wrap width = T.unpack . wrapText wrapSettings width . T.pack
 
 wrapSettings = WrapSettings {preserveIndentation = True, breakLongWords = True}
 
+-- wordWeights.txt is taken from
+-- https://en.wiktionary.org/wiki/Wiktionary:Frequency_lists#TV_and_movie_scripts
+-- (and cleaned up a little with some throwaway sed)
+wordWeights :: [(String, Int)]
+wordWeights =
+  map ((\[w, f] -> (w, read f)) . words) . lines $
+  $(embedStringFile "wordWeights.txt")
+
+totalWeight :: Int
+totalWeight = sum . map snd $ wordWeights
+
+weightedRandomWord :: IO String
+weightedRandomWord = do
+  r <- randomRIO (0, totalWeight - 1)
+  return $ go r wordWeights
+  where
+    go r ((w, f):rest)
+      | r < f = w
+      | otherwise = go (r - f) rest
+
+-- Generates nonsense which is superficially similar to English. Similar in the
+-- sense that the frequency of words in the generated text is approximately the
+-- same as the frequency of words in actual usage.
+nonsense :: Config -> IO String
+nonsense c = do
+  words <- go nonsenseLen
+  return $ (wrap (width c) . unwords $ words) ++ "\n"
+  where
+    go :: Int -> IO [String]
+    go n
+      | n <= 0 = return []
+      | otherwise = do
+        word <- weightedRandomWord
+        rest <- go (n - length word)
+        return $ word : rest
+
 sample :: Config -> String -> IO String
 sample c file =
   if paragraph c
@@ -115,126 +153,6 @@ sample c file =
       "\n"
     ascii = toAscii (tab c) file
     chop = unlines . take (height c) . lines
-
--- taken from https://en.wikipedia.org/wiki/Most_common_words_in_English
--- TODO options for expanded word lists
-topHundredWords :: [String]
-topHundredWords =
-  [ "the"
-  , "be"
-  , "to"
-  , "of"
-  , "and"
-  , "a"
-  , "in"
-  , "that"
-  , "have"
-  , "I"
-  , "it"
-  , "for"
-  , "not"
-  , "on"
-  , "with"
-  , "he"
-  , "as"
-  , "you"
-  , "do"
-  , "at"
-  , "this"
-  , "but"
-  , "his"
-  , "by"
-  , "from"
-  , "they"
-  , "we"
-  , "say"
-  , "her"
-  , "she"
-  , "or"
-  , "an"
-  , "will"
-  , "my"
-  , "one"
-  , "all"
-  , "would"
-  , "there"
-  , "their"
-  , "what"
-  , "so"
-  , "up"
-  , "out"
-  , "if"
-  , "about"
-  , "who"
-  , "get"
-  , "which"
-  , "go"
-  , "me"
-  , "when"
-  , "make"
-  , "can"
-  , "like"
-  , "time"
-  , "no"
-  , "just"
-  , "him"
-  , "know"
-  , "take"
-  , "people"
-  , "into"
-  , "year"
-  , "your"
-  , "good"
-  , "some"
-  , "could"
-  , "them"
-  , "see"
-  , "other"
-  , "than"
-  , "then"
-  , "now"
-  , "look"
-  , "only"
-  , "come"
-  , "its"
-  , "over"
-  , "think"
-  , "also"
-  , "back"
-  , "after"
-  , "use"
-  , "two"
-  , "how"
-  , "our"
-  , "work"
-  , "first"
-  , "well"
-  , "way"
-  , "even"
-  , "new"
-  , "want"
-  , "because"
-  , "any"
-  , "these"
-  , "give"
-  , "day"
-  , "most"
-  , "us"
-  ]
-
-nonsense :: Config -> IO String
-nonsense c = do
-  words <- go nonsenseLen
-  return $ (wrap (width c) . unwords $ words) ++ "\n"
-  where
-    go :: Int -> IO [String]
-    go n
-      | n <= 0 = return []
-      | otherwise = do
-        r <- randomRIO (0, length topHundredWords - 1)
-        let word = topHundredWords !! r
-        rest <- go (n - length word)
-        return $ word : rest
 
 main :: IO ()
 main = do
